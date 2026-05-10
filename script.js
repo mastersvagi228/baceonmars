fa7bdeb9be6644f6fa17058e10047162954a6d8e
mastersvagi228 <sashamatiega@gmail.com>
2026-05-10 17:26:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ function filter(type) {
 
   });
 }
+
+function updateMarsTime() {
+  const now = new Date();
+
+  const earthSeconds =
+    now.getHours() * 3600 +
+    now.getMinutes() * 60 +
+    now.getSeconds();
+
+  const marsSecondsInDay = 88775.244;
+
+  let marsSeconds = (earthSeconds / 86400) * marsSecondsInDay;
+
+  let hours = Math.floor(marsSeconds / 3600);
+  let minutes = Math.floor((marsSeconds % 3600) / 60);
+  let seconds = Math.floor(marsSeconds % 60);
+
+  document.getElementById("marsTime").innerText =
+    String(hours).padStart(2, "0") + ":" +
+    String(minutes).padStart(2, "0") + ":" +
+    String(seconds).padStart(2, "0");
+}
+
+setInterval(updateMarsTime, 1000);
+updateMarsTime();
